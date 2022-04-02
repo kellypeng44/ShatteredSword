735754af563e0a10d9b8d0971a5b70aa490b340c
Renge <renge@marmot.website>
2022-04-02 12:33:48 -0400

    feat: add MapTemplate

---
+++ src/shattered_sword/Tools/DataTypes/MapTemplate.ts
@@ -0,0 +1,15 @@
+export default class MapTemplate {
+    tilewidth: number;
+    tileheight: number;
+    entrance: RoomTemplate;
+    exit: RoomTemplate;
+    rooms: Array<RoomTemplate>;
+}
+
+export class RoomTemplate {
+    width: number;
+    height: number;
+    bottomLayer: Array<number>;
+    topLayer: Array<number>;
+    entrances: Array<[x: number, y: number]>
+}
