43f1d941deb92b57671a54547a469e85b8cd1f4a
OfficialCHenry <henry.chen.1@stonybrook.edu>
2022-04-02 21:09:01 -0400

    attack inputs added to main.ts
    may be redundant due to inputwrapper.ts

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,19 @@ import MainMenu from "./shattered_sword/Scenes/MainMenu";
     let options = {
         canvasSize: {x: 1200, y: 800},          // The size of the game
         clearColor: {r: 0, g: 0, b: 0},   // The color the game clears to
+        inputs: [
+            {name: "left", keys: ["a"]},    //TODO - add arrow keys
+            {name: "right", keys: ["d"]},
+            {name: "up", keys: ["w"]},
+            {name: "down", keys: ["s"]},
+            {name: "jump", keys: ["x","space"]},
+            {name: "attack", keys: ["j","z","enter"]},  
+            {name: "dash", keys: ["k","z"]},
+            {name: "skill", keys: ["l","v"]},
+            {name: "inventory", keys: ["i","b"]},
+            {name: "pause", keys: ["escape"]},    
+            {name: "tab", keys: ["tab"]}   
+        ],
         useWebGL: false,                        // Tell the game we want to use webgl
         showDebug: true                       // Whether to show debug messages. You can change this to true if you want
     }
@@ -19,7 +32,9 @@ import MainMenu from "./shattered_sword/Scenes/MainMenu";
     const game = new Game(options);
 
     // Start our game
-    game.start(MainMenu, {});
+    game.start(MainMenu, {});   
+    //TODO - change to splash screen once available
+    //game.start(SplashScreen,{});
 })();
 
 function runTests(){};
