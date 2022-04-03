87ff0d5ec75c6da12be40cfe2bc856bcfefb4420
OfficialCHenry <henry.chen.1@stonybrook.edu>
2022-04-03 17:43:50 -0400

    Added Player States

---
+++ src/shattered_sword/Player/PlayerStates/OnGround.ts
@@ -0,0 +1,35 @@
+import GameEvent from "../../../Wolfie2D/Events/GameEvent";
+import Input from "../../../Wolfie2D/Input/Input";
+import Sprite from "../../../Wolfie2D/Nodes/Sprites/Sprite";
+import MathUtils from "../../../Wolfie2D/Utils/MathUtils";
+import PlayerState from "./PlayerState";
+
+export default class OnGround extends PlayerState {
+	onEnter(options: Record<string, any>): void {}
+
+	update(deltaT: number): void {
+		if(this.parent.velocity.y > 0){
+			this.parent.velocity.y = 0;
+		}
+		super.update(deltaT);
+
+		let direction = this.getInputDirection();
+
+		if(direction.x !== 0){
+			(<Sprite>this.owner).invertX = MathUtils.sign(direction.x) < 0;
+		}
+
+		// If we jump, move to the Jump state, give a burst of upwards velocity
+		if(Input.isJustPressed("jump")){
+			this.finished("jump");
+			this.parent.velocity.y = -500;
+			
+		} else if(!this.owner.onGround){
+			this.finished("fall");
+		}
+	}
+
+	onExit(): Record<string, any> {
+		return {};
+	}
+}
