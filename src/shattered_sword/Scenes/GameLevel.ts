c9a2b97104a7b8706460a9f0357177cd0b988a84
Kelly Peng <mingyu.peng@stonybrook.edu>
2022-04-25 21:34:43 -0400

    add level 1 story

--- src/shattered_sword/Scenes/GameLevel.ts
+++ src/shattered_sword/Scenes/GameLevel.ts
@@ -948,7 +948,7 @@ export default class GameLevel extends Scene {
     protected playStartStory() {
         if (!this.touchedStartCheckPoint) {
             this.touchedStartCheckPoint = true;
-            this.storyLoader("shattered_sword_assets/jsons/story.json");
+            this.storyLoader("shattered_sword_assets/jsons/level1story.json");
             this.startTimer();
         }
     }
