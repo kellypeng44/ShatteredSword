1e5aa43383247eef9f86e2d67c91304cae97ade8
Renge <renge@marmot.website>
2022-04-27 22:43:44 -0400

    feat: snake apply poison damage

--- src/shattered_sword/AI/SnakeAI.ts
+++ src/shattered_sword/AI/SnakeAI.ts
@@ -1,4 +1,5 @@
 import AnimatedSprite from "../../Wolfie2D/Nodes/Sprites/AnimatedSprite";
+import PlayerController from "../Player/PlayerController";
 import EnemyAI, { EnemyStates } from "./EnemyAI";
 import SnakeAttack from "./EnemyStates/SnakeAttack";
 
@@ -6,5 +7,12 @@ export default class SnakeAI extends EnemyAI {
     initializeAI(owner: AnimatedSprite, options: Record<string, any>): void {
         super.initializeAI(owner, options);
         this.addState(EnemyStates.ATTACK, new SnakeAttack(this, owner));
+    }
+
+    collideWithPlayer(player: PlayerController): void {
+        player.damage(10);
+        if (this.isAttaking && !player.invincible && !player.godMode) {
+            player.poisonCounter = 5;
+        }
     }
 }
