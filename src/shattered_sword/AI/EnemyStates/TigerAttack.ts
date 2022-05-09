c9a983dac58154ca95856632f963dfaf2fa4e0be
Renge <renge@marmot.website>
2022-05-09 15:50:27 -0400

    fix: tiger velocity

--- src/shattered_sword/AI/EnemyStates/TigerAttack.ts
+++ src/shattered_sword/AI/EnemyStates/TigerAttack.ts
@@ -24,7 +24,7 @@ export default class TigerAttack extends Attack {
                     this.parent.isCharging = false;
                     this.parent.isAttacking = true;
                     (<AnimatedSprite>this.owner).animation.play("ATTACK", true);
-                    this.velocity = (this.parent.getPlayerPosition().x - this.owner.position.x)/1.5;
+                    this.velocity = (this.parent.getPlayerPosition().x - this.owner.position.x);
                     this.parent.direction = this.velocity >= 0 ? 1 : -1;
                     this.parent.velocity.y = -800;
                     break;
