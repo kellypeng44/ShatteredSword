68603026a5e6f648cf3d8640fb384078ae5a9896
Kelly Peng <mingyu.peng@stonybrook.edu>
2022-05-09 14:10:23 -0400

    add level 5 start story

--- src/shattered_sword/Scenes/Market.ts
+++ src/shattered_sword/Scenes/Market.ts
@@ -49,7 +49,7 @@ export default class Market extends GameLevel {
     protected playStartStory(): void {
         if (!this.touchedStartCheckPoint) {
             this.touchedStartCheckPoint = true;
-            this.storyLoader("shattered_sword_assets/jsons/level1story.json");
+            this.storyLoader("shattered_sword_assets/jsons/level5story.json");
             this.startTimer();
         }
     }
